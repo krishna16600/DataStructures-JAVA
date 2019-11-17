5c7c1fc756e6152e2dc529ec7e9c4dcb670dbe57
krishna malhotra <krishna.malhotra691692@gmail.com>
2019-11-18 02:05:03 +0530

    MOMO MARKET

---
+++ src/Recursion/MomoMarket.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class MomoMarket {
+	public static int getIndex(long[] arr,int low,int high,long x)
+	{
+		if(low > high)
+			return -1;
+		
+		if(x>=arr[high])
+			return high;
+		
+		int mid = (low+high)/2;
+		
+		if(arr[mid]==x)
+			return mid;
+		
+		if(mid > 0 && x>= arr[mid-1] && x < arr[mid])
+			return mid-1;
+		
+		if(x<arr[mid])
+			return getIndex(arr,low,mid-1,x);
+		
+		return getIndex(arr,mid+1,high,x);
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numOfShops = sc.nextInt();
+		long[] arr = new long[numOfShops];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = sc.nextLong();
+		}
+		long[] prefix = new long[numOfShops];
+		prefix[0] = arr[0];
+		for(int i=1;i<arr.length;i++)
+		{
+			prefix[i] = prefix[i-1]+arr[i];
+		}
+		int q = sc.nextInt();
+		while(q-->0)
+		{
+			long x = sc.nextLong();
+			int s = getIndex(prefix,0,numOfShops-1,x);
+			long val = x;
+			if(s!=-1)
+				val-=prefix[s];
+			System.out.println((s+1)+" "+val);
+		}
+		
+	}
+}
